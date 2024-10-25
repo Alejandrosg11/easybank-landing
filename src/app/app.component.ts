107ca53a38beb8bcfdec8470acafe1f77c97b40d
Alex Santamaria <alejandrosg11@gmail.com>
2024-10-24 20:36:44 -0600

    Mobile adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeaderComponent } from './header/header.component';
 import { BulletComponent } from './bullet/bullet.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent, BulletComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, BulletComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -10,25 +10,25 @@ import { Component } from '@angular/core';
 export class ArticlesComponent {
 	articles = [
 		{
-			"img": "string",
+			"img": "image-currency.jpg",
 			"author": "By Claire Robinson",
 			"title": "Receive money in any currency with no fees",
 			"body": "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …"
 		},
 		{
-			"img": "string",
+			"img": "image-restaurant.jpg",
 			"author": "By Wilson Hutton",
 			"title": "Treat yourself without worrying about money",
 			"body": "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …"
 		},
 		{
-			"img": "string",
+			"img": "image-plane.jpg",
 			"author": "By Wilson Hutton",
 			"title": "Take your Easybank card wherever you go",
 			"body": "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …"
 		},
 		{
-			"img": "string",
+			"img": "image-confetti.jpg",
 			"author": "By Claire Robinson",
 			"title": "Our invite-only Beta accounts are now live!",
 			"body": "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ..."
